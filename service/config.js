221f571850b9a119f937bd316f6e7b5ef463780b
cheungww <1329105041@qq.com>
2020-01-03 21:52:02 +0800

    添加项目展示

--- service/config.js
+++ service/config.js
@@ -1,3 +1,3 @@
-export const baseURL = 'http://xxx'
-// export const baseURL = 'http://xxx'
+export const baseURL = 'http://123.207.32.32:8000/api/w1'
+// export const baseURL = 'http://106.54.54.237:8000/api/w1'
 export const timeout = 5000
